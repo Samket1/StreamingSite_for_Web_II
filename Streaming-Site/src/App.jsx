9bd825a285fc295fdbc8d6d2d126a99a1708b31b
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-09-25 19:36:43 +0300

    again fix

--- Streaming-Site/src/App.jsx
+++ Streaming-Site/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
         {/* the Stream Dopamine Text */}
         <div className="nav-logo">
           <span className="logo-icon">🎬</span>
-          <span className="logo-text">Stream<span className="logo-highlight">Dopamine</span></span>
+          <span className="logo-text">Stream<span className="logo-highlight">FFDopamine</span></span>
         </div>
         {/* the search bar */}
         <div className='nav-search'>
